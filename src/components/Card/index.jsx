f143e4873d6888d125d0dcb6287f41148a6b8261
T-101 <antti.rummukainen@gmail.com>
2020-01-05 15:03:32 +0200

    prop to render divider

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,13 +4,14 @@ import { Card, Divider } from 'semantic-ui-react';
 export default class CardComponent extends React.Component {
     render() {
         const headerTextAlign = this.props.headerTextAlign || "center";
+        const divider = this.props.divider;
         return (
             <Card fluid>
                 <Card.Content>
                     <Card.Header textAlign={headerTextAlign}>
                         {this.props.header}
                     </Card.Header>
-                    {this.props.children && <Divider />}
+                    {divider && <Divider />}
                     {this.props.children}
                 </Card.Content>
             </Card>
