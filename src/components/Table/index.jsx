4e2a4b525984b6aebbb89fc0fad0614e33850985
T-101 <antti.rummukainen@gmail.com>
2020-01-14 20:57:09 +0200

    refactored calculations to utils

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -1,34 +1,8 @@
 import React from 'react';
 import { Table } from 'semantic-ui-react';
+import { getRemaining } from '../../utils'
 
 export default class TableComponent extends React.Component {
-
-    getRemaining = (precision) => {
-        const janSeconds = Number((new Date(this.props.year, 1, 1) - new Date(this.props.year, 0, 1)) / 1000).toFixed(0);
-        const millisecondsElapsed = (this.props.dateTime - new Date(this.props.year, 0, 1));
-        const secondsElapsed = Number((millisecondsElapsed / 1000)).toFixed(0);
-        const remaining = janSeconds - secondsElapsed;
-        let multiplier = 1;
-        switch (precision) {
-            case "min":
-                multiplier *= 60;
-                break;
-            case "hour":
-                multiplier *= 3600;
-                break;
-            case "day":
-                multiplier *= 86400;
-                break;
-            case "week":
-                multiplier *= 604800;
-                break;
-            default:
-                break
-        }
-        if (multiplier >= 86400) return Number(remaining / multiplier).toFixed(2);
-        return Math.floor(remaining / multiplier);
-    }
-
     render() {
 
         const tableContents = [
@@ -44,7 +18,7 @@ export default class TableComponent extends React.Component {
                 <Table.Body>
                     {tableContents.map((row, key) => (
                         <Table.Row key={key}>
-                            <Table.Cell textAlign="right">{this.getRemaining(row.short)}</Table.Cell>
+                            <Table.Cell textAlign="right">{getRemaining(this.props, row.short)}</Table.Cell>
                             <Table.Cell>{row.long}</Table.Cell>
                         </Table.Row>
                     ))}
